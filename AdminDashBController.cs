e5c6114c60d62f8d36b947d21f1453fb827ed5c4
tatajanaa <37144309+tatajanaa@users.noreply.github.com>
2020-04-15 12:06:31 +0300

    Add files via upload

---
+++ AdminDashBController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientManagement.Controllers
+{
+    public class AdminDashBController : Controller
+    {
+        // GET: AdminDashB
+        public ActionResult AdminHome()
+        {
+            return View();
+        }
+    }
+}
---
+++ CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientManagement.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Customers()
+        {
+            return View();
+        }
+    }
+}
